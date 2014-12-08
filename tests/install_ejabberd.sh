0e327ee92484f5342cce746be9121c39147c0293
Fabian Grutschus <f.grutschus@lubyte.de>
2014-12-08 17:08:27 +0100

    Redirect ejabberd installation output to /dev/null

--- tests/install_ejabberd.sh
+++ tests/install_ejabberd.sh
@@ -6,8 +6,8 @@ jabber_password=$2
 # Install ejabberd when control binary doesn't exists
 if [ -z `which ejabberdctl` ]; then
     echo -n "Install ejabberd... "
-    apt-get install -y ejabberd
-    dpkg --configure ejabberd
+    apt-get install -y ejabberd > /dev/null
+    dpkg --configure ejabberd > /dev/null
     echo "done"
 fi
 
