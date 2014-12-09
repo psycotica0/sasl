688a2147c0aec51610e6bd8299802491cc70ad9b
Fabian Grutschus <f.grutschus@lubyte.de>
2014-12-09 11:34:59 +0100

    Remove not need method

--- src/Authentication/AbstractAuthentication.php
+++ src/Authentication/AbstractAuthentication.php
@@ -74,35 +74,6 @@ abstract class AbstractAuthentication
     }
 
     /**
-     * Function which implements HMAC-SHA-1 digest
-     *
-     * @param  string $key  The secret key
-     * @param  string $data The data to hash
-     * @param  bool $raw_output Whether the digest is returned in binary or hexadecimal format.
-     * @return string       The HMAC-SHA-1 digest
-     * @author Jehan <jehan.marmottard@gmail.com>
-     * @access protected
-     */
-    protected function hmacSha1($key, $data, $raw_output = false)
-    {
-        if (strlen($key) > 64) {
-            $key = sha1($key, true);
-        }
-
-        if (strlen($key) < 64) {
-            $key = str_pad($key, 64, chr(0));
-        }
-
-        $k_ipad = substr($key, 0, 64) ^ str_repeat(chr(0x36), 64);
-        $k_opad = substr($key, 0, 64) ^ str_repeat(chr(0x5C), 64);
-
-        $inner  = pack('H40', sha1($k_ipad . $data));
-        $digest = sha1($k_opad . $inner, $raw_output);
-
-        return $digest;
-    }
-
-    /**
      * Creates the client nonce for the response
      *
      * @return string The cnonce value
