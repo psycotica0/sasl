1152d6adfc2577fde7ab45a5a26fdb433e1f90f9
Fabian Grutschus <f.grutschus@lubyte.de>
2014-12-18 13:31:07 +0100

    Put after_script into own script

---
+++ tests/upload_artefacts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ -n $IS_HHVM && $IS_HHVM -eq 0 ]]; then
+    wget https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover build/logs/clover.xml
+    php vendor/bin/coveralls
+    php vendor/bin/test-reporter --stdout > codeclimate.json
+    curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' \
+        -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' \
+        https://codeclimate.com/test_reports
+fi
